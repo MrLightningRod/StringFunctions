f09971504f2f536c952a9e78ee8d3c675912e38b
MrLightningRod <ivan_nasedkin97@mail.ru>
2015-09-30 22:25:53 +0300

    Почти
    Добавил strtod

--- StringFunctions.c
+++ StringFunctions.c
@@ -21,6 +21,28 @@ int OneMoreIsspace(int ch)
 	return (!a);
 }
 
+int OneMoreIsdigit(int ch)
+{
+	assert(ch <= MY_CHAR_MAX);
+	assert(ch >= MY_CHAR_MIN);
+	return (('0' <= ch) && (ch <= '9'));
+}
+
+double DecimalPower(int power)
+{
+	double res = 1.0;
+	double base = 0.0;
+	if (power > 0) base = 10.0;
+	if (power < 0)
+	{
+		power = -power;
+		base = 0.1;
+	}
+	int i = 0;
+	for (i; i < power; i++) res *= base;
+	return res;
+}
+
 int OneMoreUpcase(int ch)
 {
 	assert((MY_CHAR_MIN <= ch) && (ch <= MY_CHAR_MAX));
@@ -131,7 +153,75 @@ unsigned long int OneMoreStrtoul(const char *string, char **endptr, int basis)
 	return res;
 }
 
-int main()
+double OneMoreStrtod(const char *string, char **endptr)
 {
-	return 0;
+	double result = 0.0;
+	double basis = 10.0;
+	int sign = 1;
+	while (OneMoreIsspace(*string)) string++;
+	if (*string == '+')
+	{
+		string++;
+	}
+	else if (*string == '-')
+	{
+		sign = -1;
+		string++;
+	}
+	int was_digit = 0;
+	int was_digit_dot = 0;
+	int was_dot = 0;
+	int was_E = 0;
+	int was_E_sign = 0;
+	int E_power = 0;
+	int E_sign = 1;
+	double power_after_dot = 0.1;
+	while (*string)
+	{
+		if (OneMoreIsdigit(*string))
+		{
+			if (was_E)
+			{
+				E_power = E_power * basis + (*string - '0');
+			}
+			else if (was_dot)
+			{
+				was_digit_dot = 1;
+				result += (*string - '0') * power_after_dot;
+				power_after_dot /= basis;
+			}
+			else
+			{
+				was_digit = 1;
+				result = result * basis + (*string - '0');
+			}
+		}
+		else if (*string == '.')
+		{
+			if (!was_digit) break;
+			if (was_dot) break;
+			was_dot = 1;
+		}
+		else if (OneMoreUpcase(*string) == 'E')
+		{
+			if (((was_dot) && (!was_digit_dot)) || (!was_digit)) break;
+			if (was_E) break;
+			was_E = 1;
+			was_dot = 1;
+		}
+		else if ((*string == '-') || (*string == '+'))
+		{
+			if (!was_E) break;
+			if (was_E_sign) break;
+			was_E_sign = 1;
+			if (*string == '-') E_sign = -1;
+		}
+		else break;
+		string++;
+	}
+	E_power *= E_sign;
+	result *= DecimalPower(E_power);
+	if (result != 0.0) result *= sign;
+	if (endptr != NULL) *endptr = string;
+	return result;
 }
